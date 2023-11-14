b47d57903af2007c7dc5f601fc4ab8a7ee4b8442
William Reeder <whhriv@gmail.com>
2023-11-13 17:40:49 -0800

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+           
+          </Nav>
+   
+      </Container>
+    </Navbar>
+  );
+}
